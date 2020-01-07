4f19f82a2731e93984d7d82f6537ccdbdda3a4ac
peter kuria <you@example.com>
2020-01-07 04:06:14 -0500

    created a new function ansanews to get news articles from ansa

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     return render_template('index.html')
 @app.route('/bbc')
 def Bbc():
-    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
+    newsapi = NewsApiClient(api_key="40d18f3377d342d4a16854c146595ff9")
     topheadlines = newsapi.get_top_headlines(sources="bbc-news")
 
     articles = topheadlines['articles']
@@ -15,6 +15,8 @@ def Bbc():
     news = []
     desc = []
     img = []
+    date=[]
+    urls=[]
 
     for i in range(len(articles)):
         myarticles = articles[i]
@@ -22,9 +24,41 @@ def Bbc():
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
+        date.append(myarticles['publishedAt'])
+        urls.append(myarticles['url'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, desc, img,date,urls)
 
     return render_template('bbc.html', context = mylist)
+
+app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/ansa')
+def Ansanews():
+    newsapi = NewsApiClient(api_key="40d18f3377d342d4a16854c146595ff9")
+    topheadlines = newsapi.get_top_headlines(sources="ansa")
+
+    articles = topheadlines['articles']
+
+    news = []
+    desc = []
+    img = []
+    date=[]
+    urls=[]
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        date.append(myarticles['publishedAt'])
+        urls.append(myarticles['url'])
+
+    mylist = zip(news, desc, img,date,urls)
+
+    return render_template('ansa.html', context = mylist)
+
 if __name__ == "__main__":
     app.run(debug = True)
