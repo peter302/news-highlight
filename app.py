1f36b3d6f94753db05b1a5d51c212d017b1ea35b
peter kuria <you@example.com>
2020-01-07 03:44:07 -0500

    added a new function to get news from bbc

--- app.py
+++ app.py
@@ -5,3 +5,24 @@ app = Flask(__name__)
 app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/bbc')
+def Bbc():
+    newsapi = NewsApiClient(api_key="c40d6d5c48504741980b4e27910dd4a4")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines['articles']
+
+    news = []
+    desc = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, img)
+
+    return render_template('bbc.html', context = mylist)
