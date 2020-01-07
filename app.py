14f5ecf84384da17284b1180f732108003d758ee
peter kuria <you@example.com>
2020-01-07 06:18:25 -0500

    added a function,aust to fetch data from australian news"

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 from newsapi import NewsApiClient
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/bbc')
@@ -85,7 +85,34 @@ def Axio():
     mylist = zip(news, desc, img,date,urls)
 
     return render_template('aljazeera.html', context = mylist)
-    
+
+
+@app.route('/aust')
+def Aust():
+    newsapi = NewsApiClient(api_key="40d18f3377d342d4a16854c146595ff9")
+    topheadlines = newsapi.get_top_headlines(sources="australian-financial-review")
+
+    articles = topheadlines['articles']
+
+    news = []
+    desc = []
+    img = []
+    date=[]
+    urls=[]
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        date.append(myarticles['publishedAt'])
+        urls.append(myarticles['url'])
+
+    mylist = zip(news, desc, img,date,urls)
+
+    return render_template('aust.html', context = mylist)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
